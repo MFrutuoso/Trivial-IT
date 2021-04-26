9ddf9437b3854e3b77c3d447e3b959b2d5a18655
mfrutuoso <mfrutuososv@gmail.com>
2021-04-26 20:35:08 +0200

    Comienzo del proyecto

---
+++ Trivial-IT/app/src/main/java/com/dam2/trivial_it/PActivas.java
@@ -0,0 +1,14 @@
+package com.dam2.trivial_it;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PActivas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_activas);
+    }
+}
