bc35bc790ea120e270259d640bcb8fe2c66c3f55
MFrutuoso <mfrutuososv@gmail.com>
2021-05-26 16:58:13 +0200

    Servicio de musica corregido

--- Trivial-IT/app/src/main/java/com/dam2/trivial_it/ServicioMusica.java
+++ Trivial-IT/app/src/main/java/com/dam2/trivial_it/ServicioMusica.java
@@ -2,15 +2,52 @@ package com.dam2.trivial_it;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 
 public class ServicioMusica extends Service {
-    public ServicioMusica() {
+
+    private MediaPlayer miReproductor;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        miReproductor = null;
+
+        miReproductor = MediaPlayer.create(this, R.raw.fondo);
+        miReproductor.setLooping(true);
+        miReproductor.setVolume(100f,100f);
+
+    }
+
+    ///Iniciamos el servicio
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        //return super.onStartCommand(intent, flags, startId);
+        if(miReproductor.isPlaying() == false){
+            if(miReproductor != null)
+                miReproductor.start();
+        }
+        return START_STICKY;
+
+    }
+
+    //Paramos el servicio
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(miReproductor.isPlaying() == true) miReproductor.stop();
+        miReproductor.release();
+        miReproductor = null;
+
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 }
